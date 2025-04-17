63bf5340ee2b3b43ffb05f19fca90ae2964b8b67
xarop <ajl@xarop.com>
2025-04-17 16:14:33 +0200

    update

--- themes/xarop-theme/footer.php
+++ themes/xarop-theme/footer.php
@@ -1,6 +1,6 @@
 <footer class="site-footer">
     <div class="container">
-        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php esc_html_e('All rights reserved.', 'xarop-theme'); ?></p>
+        &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php esc_html_e('All rights reserved.', 'xarop-theme'); ?>
     </div>
 </footer>
 <?php wp_footer(); ?>
